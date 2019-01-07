c3a39a5c12d90f4b33b2b0065db680ed9197912b
Tom Howard <tom@windyroad.com.au>
2019-01-08 07:52:58 +1100

    Initial Commit

---
+++ Mobile Router Status/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mobile Router Status
+//
+//  Created by Tom Howard on 8/1/19.
+//  Copyright © 2019 Mountain Pass. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mobile Router Status/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mobile Router Status
+//
+//  Created by Tom Howard on 8/1/19.
+//  Copyright © 2019 Mountain Pass. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
